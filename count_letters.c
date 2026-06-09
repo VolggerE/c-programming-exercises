3b9adbb33a38908a2ffe90c4cde714bd21713f0d
VolggerE <drvolgger@outlook.com>
2026-06-09 14:24:08 +0200

    Update and rename s06e03.c to count_letters.c

--- count_letters.c
+++ count_letters.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-size_t count_number_of_letters(char text[]) {
+size_t count_letters(const char text[]) {
 size_t counter = 0;
     for (int i = 0; text[i] != '\0'; i++)
     {
@@ -22,10 +22,10 @@ char e3[] = {"abcABC"};
 char e4[] = {"a!B?"};
 char e5[] = {"1a 2b 3c"};    
 
-printf("The string \"%s\" has %ld letter(s)\n", e1, count_number_of_letters(e1));
-printf("The string \"%s\" has %ld letter(s)\n", e2, count_number_of_letters(e2));
-printf("The string \"%s\" has %ld letter(s)\n", e3, count_number_of_letters(e3));
-printf("The string \"%s\" has %ld letter(s)\n", e4, count_number_of_letters(e4));
-printf("The string \"%s\" has %ld letter(s)\n", e5, count_number_of_letters(e5));
+printf("The string \"%s\" has %zu letter(s)\n", e1, count_letters(e1));
+printf("The string \"%s\" has %zu letter(s)\n", e2, count_letters(e2));
+printf("The string \"%s\" has %zu letter(s)\n", e3, count_letters(e3));
+printf("The string \"%s\" has %zu letter(s)\n", e4, count_letters(e4));
+printf("The string \"%s\" has %zu letter(s)\n", e5, count_letters(e5));
     return EXIT_SUCCESS;
 }
